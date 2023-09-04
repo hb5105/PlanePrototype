e987edb15305977ba6eb655204e723ef0c8ddd67
Hritik Ajay Bansal <43716075+hb5105@users.noreply.github.com>
2023-09-04 12:59:17 -0700

    plane-prototype

---
+++ Assets/Challenge 1/Scripts/SpinPropellarX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float propellorSpeed = 1000;
+    void Start() { }
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+    }
+}
